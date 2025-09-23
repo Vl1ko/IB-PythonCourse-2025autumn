73074ef8b30e7faf3e3520db169ff3fde65b040a
Ana-Raf99 <RafeenkoAA25@spb.ithub.ru>
2025-09-23 19:44:23 +0300

    Create RafeenkoAnastasia-CP2.py

---
+++ checkpoint-2/RafeenkoAnastasia-CP2.py
@@ -0,0 +1,32 @@
+#Задание 1
+birth = int(input("Введите ваш год рождения: "))
+year_now = 2025
+user_age = year_now - birth
+age_after = user_age + 10 
+print(f"Ваш текущий возраст: {user_age}")
+print(f"Через 10 лет вам будет: {age_after}","\n")
+      
+
+# Задание 2
+energy = int(input("Введите целое число: "))
+if energy % 2 == 0:
+    print(f"Число {energy} является четным.")
+else:
+    print(f"Число {energy} является нечетным.","\n")
+
+# Задание 3 
+num = int(input("Введите исходные координаты:"))
+print(f"Исходное число: {num}")
+print(f"Побитовый сдвиг влево на 2 (10 << 2): {num} << 2")
+print(f"Побитовый сдвиг вправо на 1 (10 >> 1): {num} >> 1","\n")
+
+#Задание 4
+register = int(input("Введите значение системного регистра:"))
+bit_number = int(input("Введите номер бита для проверки (начиная с 0): "))
+if bit_number < 0 or bit_number > 31:
+    print(f"ошибка")
+if register & (1 << bit_number):
+    print(f"Бит {bit_number} установлен (значение 1)")
+else: 
+    print(f"Бит {bit_number} сброшен (значение 0)", "\n")
+
