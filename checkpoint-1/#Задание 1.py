c1c90f240aba0bef072a9d360a941bfeadbc57ad
Ana-Raf99 <RafeenkoAA25@spb.ithub.ru>
2025-09-23 16:49:30 +0300

    Add files via upload

---
+++ checkpoint-1/#Задание 1.py
@@ -0,0 +1,37 @@
+#Задание 1
+name = "Рафеенко Анастасия"
+print(name)
+age = "26"
+print(age)
+number = 3.14159
+print(number)
+print("True")
+
+#Задание 2
+color = "черный"
+print("Ваш любимый цвет:", color)
+day = "1"
+print("Ваш опыт в программировании", day)
+color = "черный"
+day = "1"
+print("Привет, я люблю", color, "цвет и программирую уже", day, "день")
+
+#Задание 3
+print("2025", "09", "13", sep='/')
+print("Python", "is great", sep='_')
+
+#Задание 4
+word = input("Введите любое слово: Привет")
+print("Вы ввели:", word)
+print("Тип переменной слово:", type(word))  
+number = "40"
+print("Тип переменной число:", type(number))
+number = int(number)
+print("Тип переменной число:", type(number))
+
+
+#Задание 5
+print(bool(""))
+print(bool(" "))
+print(bool(0))
+print(bool(-10))
