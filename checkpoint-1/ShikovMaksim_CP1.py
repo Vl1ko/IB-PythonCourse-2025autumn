e2dd6936be8c59326160299ea8147ffb4b708ea6
Shikov Maksim <daavold@gmail.com>
2025-09-25 20:41:03 +0300

    Обновил код

--- checkpoint-1/ShikovMaksim_CP1.py
+++ checkpoint-1/ShikovMaksim_CP1.py
@@ -4,28 +4,28 @@ Pi=3.14159
 print("Моё имя Максим")
 print("Мне", age1, "год")
 print("Число Пи равно", Pi)
-print(True, "\n")
+print(True, end=("\n\n"))
 
 #Задача 2
 color=input("Какой ваш любимый цвет?")
-age2=input("Сколько лет вы занимаетесь програмированнием?")
+age2=int(input("Сколько лет вы занимаетесь програмированнием?"))
 print("Какой ваш любимый цвет?")
 print("Сколько лет вы занимаетесь програмированнием?")
-print("Привет, я люблю", color, "цвет и программирую уже", age2, "лет!\n")
+print("Привет, я люблю", color, "цвет и программирую уже", age2, "лет!", end=("\n\n"))
 
 
 #Задача 3
 print("Сегодня", end=" ")
 print("07" ,"15", "2024", sep="/")
 print("Python", end="_")
-print("is great\n")
+print("is great", end=("\n\n"))
 
 #Задача 4
 word="Просто"
 num1=3020
 print("Скажите любое слово и любое целое число")
 print("Тип переменной целого числа:", num1, type(num1))
-print("Тип переменной слова:", word, type(word), "\n")
+print("Тип переменной слова:", word, type(word), end=("\n\n"))
 
 #Задача 5
 empty_stroka1=""
@@ -35,4 +35,4 @@ num3=-10
 print("Результат преобразования empty_stroka1:", bool(empty_stroka1))
 print("Результат преобразования empty_stroka2:", bool(empty_stroka2))
 print("Результат преобразования num2:", bool(num2))
-print("Результат преобразования num3:", bool(num3))
+print("Результат преобразования num3:", bool(num3), end=("\n\n"))
--- checkpoint-2/ShikovMaksim_CP2.py
+++ checkpoint-2/ShikovMaksim_CP2.py
@@ -4,20 +4,20 @@ year_now = 2025
 years_now = year_now - user_year
 years_after = years_now + 10
 print("Ваш текущий возвраст:", years_now)
-print("Через 10 лет вам будет:", years_after, "\n")
+print("Через 10 лет вам будет:", years_after, end=("\n\n"))
 
 #Задание 2
 shield_energy = int(input("Введиет уровень энергии: "))
 if shield_energy % 2 == 0:
     print("Щит активен")
 else:
-    print("Щит не активен\n")
+    print("Щит не активен", end=("\n\n"))
 
 #Задание 3
 start_cord = int(input("Введите исходные координаты: "))
 print("Исходное число:", start_cord)
 print("Побитывай сдвиг влево на 2 (10 << 2):", start_cord<<2)
-print("Побитовый сдвиг в право на 1 (10 >> 1):", start_cord>>1, "\n")
+print("Побитовый сдвиг в право на 1 (10 >> 1):", start_cord>>1, end=("\n\n"))
 
 #Задание 4
 sys_reg = int(input("Введите значение системного регистра: "))
@@ -25,4 +25,4 @@ sys_byt = int(input("Введите номер бита для проверки:
 if sys_reg &(1<<sys_byt):
     print("Бит", sys_byt, "установлен (1)")
 else:
-    print("Бит", sys_byt, "сброшен (0)")
+    print("Бит", sys_byt, "сброшен (0)", end=("\n\n"))
