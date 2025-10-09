77bce8ea36739b07a424ba1fa5e036ad60b759fe
UnderDK2 <ledi.di.koyvi@yandex.ru>
2025-10-09 20:59:42 +0300

    Добавил работу ИТ1

---
+++ control-1/Itog1_KDD.py
@@ -0,0 +1,8 @@
+# Задание 1
+a = int(input("Введите число:"))
+b = int(input("Введите второе число:"))
+sum = a + b
+difference = a - b
+multiplication = a * b
+division = a / b
+print("Сумма: a+b =", sum, '\n', "Разность: a - b =", difference, '\n', "Произведение: a * b =", multiplication, '\n', "Частное: a / b =", division)
